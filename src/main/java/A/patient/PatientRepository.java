ae8ee86bf9672bbe15813ec639a3431a60d34a20
AKIF <AKIF@DESKTOP-F95RQ1U>
2017-08-25 23:08:46 +0600

    Doctor, Patient CRUD operations done using RDBMS

---
+++ src/main/java/A/patient/PatientRepository.java
@@ -0,0 +1,9 @@
+package A.patient;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PatientRepository extends CrudRepository<Patient, String> {
+
+   
+}
--- src/main/java/A/patient/PatientService.java
+++ src/main/java/A/patient/PatientService.java
@@ -4,54 +4,41 @@ package A.patient;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
-        
-        private List<Patient> L = new ArrayList<Patient> (Arrays.asList(
-                new Patient("01","Sabit Hasan","Dhanmondi, Dhaka","01720912970","sabit@gmail.com"),
-                new Patient("02","Jubayer Al Mahmud","Hatirpool, Dhaka","01730912971","fahim@gmail.com"),
-                new Patient("03","Shamsul Islam","Gulshan, Dhaka","01740912973","islam@gmail.com"),
-                new Patient("04","Jawad Iqbal","Banani, Dhaka","01750912974","jawad@gmail.com"),
-                new Patient("05","Rubel Rony","Farmgate, Dhaka","01716912975","rony@gmail.com")
-        ));
-        
-     
+    
+    @Autowired
+    private PatientRepository pr;
+
         public List<Patient> getAllPatients()
         {  
-            return L;
+            List<Patient> t = new ArrayList<Patient>();
+            pr.findAll()
+                    .forEach(t :: add);
+            return t;
         }
         
         public Patient getPatient(String id)
         {
-            return L.stream().filter(t -> t.getP_id().equals(id)).findFirst().get();
+            return pr.findOne(id);
         }
 
     void addPatient(Patient p) 
     {
-        L.add(p);
+        pr.save(p);
     }
 
     void updatePatient(Patient p, String id) 
     {
-        for(int i = 0; i < L.size(); i++)
-        {
-            Patient a = (Patient)L.get(i);
-            
-            if(a.getP_id().equals(id))
-            {
-                L.set(i, p);
-                return;
-            }
-        }
-    
+        pr.save(p);
     }
 
     void deletePatient(String id) 
     {
-        L.removeIf(t -> t.getP_id().equals(id));
-    
+        pr.delete(id);
     }
         
 }
