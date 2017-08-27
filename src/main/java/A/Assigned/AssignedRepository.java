0fd075ebde8cdde4cd9bbb352a982235893c22cf
AKIF <AKIF@DESKTOP-F95RQ1U>
2017-08-27 22:12:35 +0600

    All done! Using SecurityConfig.java to handle security

--- src/main/java/A/Assigned/AssignedRepository.java
+++ src/main/java/A/Assigned/AssignedRepository.java
@@ -3,15 +3,15 @@ package A.Assigned;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-//@PreAuthorize("hasRole('ROLE_USER')")
+@PreAuthorize("hasRole('ROLE_USER')")
 public interface AssignedRepository extends CrudRepository<Assigned, String> 
 {
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
-   // @Override
-    //Assigned save(Assigned s);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    Assigned save(Assigned s);
 
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
-    //@Override
-   // void delete(String id);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(String id);
   
 }
